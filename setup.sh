b8e56ff5ec476e7125da919e2b6443bee7aa128f
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-02-11 17:07:59 +0100

    fix path, rename file

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 sudo cp workdirfs.service /etc/systemd/user/workdirfs.service
-sudo mkdir -pv /etc/systemd/user/default.target.wants/workdirfs.service
+sudo mkdir -pv /etc/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service /etc/systemd/user/default.target.wants/workdirfs.service
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
