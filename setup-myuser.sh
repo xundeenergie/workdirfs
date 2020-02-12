e694b5ea4faf27a48c1eb8b0c595d9a2a0c0f51a
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-02-12 18:40:38 +0100

    fix removing config-file

--- setup-myuser.sh
+++ setup-myuser.sh
@@ -4,7 +4,5 @@ sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 cp workdirfs.service ~/.config/systemd/user/workdirfs.service
 sudo mkdir -pv ~/.config/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
-cp workdirfs.conf ~/.config/workdirfs.conf
-sudo cp workdirfs.conf /etc/workdirfs.conf
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 sudo cp workdirfs.service /etc/systemd/user/workdirfs.service
 sudo mkdir -pv /etc/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service /etc/systemd/user/default.target.wants/workdirfs.service
-sudo cp workdirfs.conf /etc/workdirfs.conf
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
