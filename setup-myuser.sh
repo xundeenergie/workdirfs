a1b772a2962bca64a2c7c5687a20e1f91afebee8
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-02-12 18:42:39 +0100

    fix wrong usage of sudo

--- setup-myuser.sh
+++ setup-myuser.sh
@@ -2,7 +2,7 @@
 
 sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 cp workdirfs.service ~/.config/systemd/user/workdirfs.service
-sudo mkdir -pv ~/.config/systemd/user/default.target.wants/
-sudo ln -sf ../workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
+mkdir -pv ~/.config/systemd/user/default.target.wants/
+ln -sf ../workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
