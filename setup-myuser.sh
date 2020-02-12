da180df2308c32234346cb56dfcb6935e1d09020
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-02-12 17:32:53 +0100

    fix filenames to copy

--- setup-myuser.sh
+++ setup-myuser.sh
@@ -4,7 +4,7 @@ sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 cp workdirfs.service ~/.config/systemd/user/workdirfs.service
 sudo mkdir -pv ~/.config/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
-cp workdirfs.config ~/.config/workdirfs.conf
-sudo cp workdirfs.config /etc/workdirfs.conf
+cp workdirfs.conf ~/.config/workdirfs.conf
+sudo cp workdirfs.conf /etc/workdirfs.conf
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 sudo cp workdirfs.service /etc/systemd/user/workdirfs.service
 sudo mkdir -pv /etc/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service /etc/systemd/user/default.target.wants/workdirfs.service
-sudo cp workdirfs.config /etc/workdirfs.conf
+sudo cp workdirfs.conf /etc/workdirfs.conf
 systemctl --user daemon-reload
 systemctl --user restart workdirfs.service
