b18e8042b5cc7fb367a4d417097f8d20ecf6bef8
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-02-12 17:30:08 +0100

    change configs

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,6 @@ echo "Remove workdirfs"
 echo "stop workdirfs.service"
 systemctl --user stop workdirfs.service
 echo "remove binaries and units from system"
-sudo rm -rf /usr/local/bin/workdirfs.py /etc/systemd/user/workdirfs.service /etc/systemd/user/default.target.wants/workdirfs
+sudo rm -rf /usr/local/bin/workdirfs.py /etc/workdirfs.conf /etc/systemd/user/workdirfs.service /etc/systemd/user/default.target.wants/workdirfs
 echo "remove systemd-units for this user $USER"
-rm -rf ~/.config/systemd/user/workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
+rm -rf ~/.config/workdirfs.conf ~/.config/systemd/user/workdirfs.service ~/.config/systemd/user/default.target.wants/workdirfs.service
