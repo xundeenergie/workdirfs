42e6c9a1c3af8a2133dfdfccfee976f576c2cbe7
Jakobus Schürz <jakobus.schuerz@schuerz.at>
2020-03-22 05:43:40 +0100

    set fusermount systemspecific

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+FUSERMOUNT=$(which fusermount)
+echo $FUSERMOUNT
 . ./uninstall.sh
 
 echo "Install workdirfs for all users"
 sudo cp workdirfs.py /usr/local/bin/workdirfs.py
 sudo cp workdirfs.service /etc/systemd/user/workdirfs.service
+echo replace 
+sudo sed -i -e "s@%FUSERMOUNT%@${FUSERMOUNT}@" /etc/systemd/user/workdirfs.service
+echo replaced
 sudo mkdir -pv /etc/systemd/user/default.target.wants/
 sudo ln -sf ../workdirfs.service /etc/systemd/user/default.target.wants/workdirfs.service
 systemctl --user daemon-reload
